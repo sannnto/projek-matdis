369b861c20b98bcfb72c7d8c6be0d26c7999f4b3
sannnto <ahmadirsantohibatullah@gmail.com>
2024-12-19 11:27:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById("encryptButton").addEventListener("click", function() {
-    const inputText = document.getElementById("inputText").value;
-    const key = 13;
-    const encryptedText = caesarCipher(inputText, key);
-    document.getElementById("outputText").value = encryptedText;
+document.getElementById("encryptButton").addEventListener("click", function() 
+{const inputText = document.getElementById("inputText").value;
+const key = 15;
+const encryptedText = caesarCipher(inputText, key);
+document.getElementById("outputText").value = encryptedText;
 });
 
 function caesarCipher(str, shift) {
@@ -26,3 +26,32 @@ function caesarCipher(str, shift) {
         return char; // Jika bukan huruf, kembalikan karakter aslinya
     }).join('');
 }
+
+document.getElementById("decryptButton").addEventListener("click", function()
+{const inputText = document.getElementById("inputText").value;
+    const key = 11;
+    const encryptedText = caesarCipher(inputText, key);
+    document.getElementById("outputText").value = encryptedText;
+    });
+    
+    function caesarCipher(str, shift) {
+        return str.split('').map(char => {
+            if (char.match(/[a-z]/i)) {
+                const code = char.charCodeAt();
+                let shiftedCode = code + shift;
+    
+                if (char >= 'a' && char <= 'z') {
+                    if (shiftedCode > 122) {
+                        shiftedCode -= 26;
+                    }
+                } else if (char >= 'A' && char <= 'Z') {
+                    if (shiftedCode > 90) {
+                        shiftedCode -= 26;
+                    }
+                }
+    
+                return String.fromCharCode(shiftedCode);
+            }
+            return char; // Jika bukan huruf, kembalikan karakter aslinya
+        }).join('');
+    }
